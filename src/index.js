0e6205c301c3f2cf2c25c7333c61f44cb0004c3f
lau-yt <laura.y.lp96@gmail.com>
2022-05-21 14:52:47 -0300

    comentario

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ var marker, circle,id;
 const options = {
     enableHighAccuracy: true,
     maximumAge: 30000,
-    timeout: 10000
+    //timeout: 10000
 };
 document.getElementById("button").addEventListener('click', ()=>{
     navigator.geolocation.clearWatch(id);
