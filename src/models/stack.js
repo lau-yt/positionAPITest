a2a04626aa2241dce673e22e695c5f3da4c34f48
infosergio2020 <infosergio2020@gmail.com>
2022-08-11 20:31:48 -0300

    reestructurando el back

---
+++ src/models/stack.js
@@ -0,0 +1,28 @@
+export class Pila {
+    elementos = [];
+    top = ()=> {
+        return this.elementos[this.elementos.length-1];
+    }
+    push = (elemento) => {
+        return this.elementos.push(elemento);
+    }
+    pop = () =>{
+        return this.elementos.pop();
+    }
+    esVacio = ()=>{
+        return (this.elementos.length == 0);
+    }
+    vaciar = ()=>{
+        this.elementos.length == 0;
+    }
+    tamanio = ()=>{
+        return this.elementos.length;
+    }
+    toString = ()=>{
+        var char="";
+        for(let i=0;i<this.elementos.length;i++){
+            char=char+this.elementos[i].numero+this.elementos[i].visitado;
+        }
+        return char;
+    }
+}
