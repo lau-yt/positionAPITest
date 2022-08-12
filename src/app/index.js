88511bf6501e0d1bffb7925c20adff2c1763104a
infosergio2020 <infosergio2020@gmail.com>
2022-08-11 21:19:58 -0300

    nueva rama

--- src/app/index.js
+++ src/app/index.js
@@ -1,15 +1,7 @@
 var area_global;
 var area_novisitado;
 
-var map = L.map('map').setView([-34.884032, -58.019961], 20);
-var tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-    maxZoom: 18,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1
-}).addTo(map);
+import { map } from "../models/map.js";
 
 import { Pila } from "../models/stack.js";
 
---
+++ src/models/map.js
@@ -0,0 +1,9 @@
+export var map = L.map('map').setView([-34.883919, -58.019961], 19);
+var tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    maxZoom: 19,
+    // attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+    //     'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+}).addTo(map);
